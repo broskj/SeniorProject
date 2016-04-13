2b992ebf89b9cf9b5772013f77291bdbc3b63c63
Kyle Brost <kjbrost@gmail.com>
2016-04-13 05:39:48 -0400

    finished lights!

--- lights.example.py
+++ lights.example.py
@@ -19,7 +19,15 @@
 		the GPIO is ass-backwards and it thinks False means 'turn on'.  
 """
  
-"""	
+"""
+Use cases:
+	User toggles status on site
+		read in loop
+		toggle new_light status to reflect
+		
+		
+		
+		
 	User hits light switch
 		light is currently on:
 			turn off
@@ -46,27 +54,36 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 
+
 baseUrl = 'http://broskj.me/api'
 loginUrl = 'http://broskj.me/auth/local'
 baseLightsUrl = baseUrl + '/Lights'
 baseLogsUrl = baseUrl + '/LightsLog'
 
-# get authorization token to assign to headers
-token = json.loads(req.post(loginUrl, data={'email':'username@emain.com','password':'mypassword'}).text)['token']
-header = {'Authorization':'Bearer ' + str(token)}
+try:
+	# get authorization token to assign to headers
+	token = json.loads(req.post(loginUrl, data={'email':'email@email.com','password':'mypassword'}).text)['token']
+	header = {'Authorization':'Bearer ' + str(token)}
+except req.exceptions.ConnectionError:
+	print "Connection refused"
 
 # gpio pins
 count = 4
 sleep_time = 0.25
 pir_in = 18
-init_gpio_in = [22,23,24,25]
-init_gpio_out = [2,3,4,17]
+init_gpio_in = [25,24,22,23]
+init_gpio_out = [17,4,2,3]
 cur_motion = False
 off_time = datetime.datetime.now()
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(pir_in, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(init_gpio_in, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup(init_gpio_out, GPIO.OUT, initial=GPIO.HIGH)
+
+# get initial api data
+lights = json.loads(req.get(baseLightsUrl, headers=header).text)
 
 # initialize light arrays
 lights_list = [None for x in range(count)]
@@ -86,73 +103,88 @@ class Light(object):
 		
 	# define behavior for detecting changes in api
 	# update local variables and toggle gpio if needed - 
-	def api_toggle(self, newStatus=None, newMotion=None, newTimed=None, newOnTime=None, newOffTime=None:
-		var now = datetime.datetime.now()
-		h = now.hour
-		m = now.minute
+	def api_toggle(self, newStatus=None, newMotion=None, newTimed=None, newOnTime=None, newOffTime=None):
 		if newStatus is not None:
 			# update status and set GPIO
+			#if self.status is not newStatus:
+			#print 'toggling status'
 			self.status=newStatus
-			GPIO.output(self.gpio_out, self.status is 'false')
+			#if self.timed and not newStatus:
+			#	self.timed=False
+			#	print 'test'
+			#	req.put(baseLightsUrl + '/' + self.id, data={'status':newStatus, 'timed':self.timed, 'updatedAt':str(datetime.datetime.now())}, headers=header)
+			GPIO.output(self.gpio_out, not self.status)
 		elif newMotion is not None:
 			# update motion and set GPIO if there is motion
 			self.motion=newMotion
-			if cur_motion:
-				self.status='true'
-				GPIO.output(self.gpio_out, self.status is 'false')
 		elif newTimed is not None:
 			# update timed and set GPIO if timed and in time range
 			self.timed=newTimed
-			if self.timed and self.inTimeRange(h, m):
-				self.status='true'
-				GPIO.output(self.gpio_out, self.status is 'false')
 		elif newOnTime is not None:
 			# update onTime and set GPIO if timed and in time range
 			self.ontime=newOnTime
-			if self.timed and self.inTimeRange(h,m):
-				self.status='true'
-				GPIO.output(self.gpio_out, self.status is 'false')
 		elif newOffTime is not None:
 			# update offTime and set GPIO if timed and in time range
 			self.offTime=newOffTime
-			if self.timed and self.inTimeRange(h,m):
-				self.status='true'
-				GPIO.output(self.gpio_out, self.status is 'false')
 		else:
 			print 'something happened'
 			
 	# define behavior for detecting physical actions
 	# need to toggle GPIO to reflect status
 	def physical_toggle(self, newStatus=None):
-		var now = str(datetime.datetime.now())
+		print 'physical toggle'
+		self.status=newStatus
+		now = str(datetime.datetime.now())
 		# only called on button press - only concerned with toggling light status
 		if newStatus is not None:
 			# disable timed if turned off while in time range
-			if self.timed and self.inTimeRange(datetime.datetime.now().hour, datetime.datetime.now().minute) and newStatus is 'false':
-				self.timed='false'
-				req.put(baseLightsUrl + '/' + self.id, data={'status':newStatus, 'timed':'false', 'updatedAt':now}, headers=header)
+			if self.timed and self.inTimeRange() and newStatus is False:
+				print 'attempting to toggle off while on timer'
+				self.timed=False
+				req.put(baseLightsUrl + '/' + self.id, data={'status':newStatus, 'timed':self.timed, 'updatedAt':now}, headers=header)
 				# need to POST to log with description
 			else:
-				req.put(baseLightsUrl + '/' + self.id, data={'status':newStatus,'updatedAt':now}, headers=header)
-			self.status=newStatus
-			GPIO.output(self.gpio_out, newStatus is 'false') # newStatus is 'false' returns boolean
+				print 'toggling ' + self.description + ' to ' + str(newStatus)
+				t = req.put(baseLightsUrl + '/' + self.id, data={'status':self.status,'updatedAt':now}, headers=header)
+				print t.text
+			# GPIO.output(self.gpio_out, newStatus is False) # newStatus is 'false' returns boolean
 			
+	def toggle_from_button(self, newStatus=None):
+		now = str(datetime.datetime.now())
+		print 'toggle from button'
+		if newStatus is None:
+			if not self.status:
+				newStatus = 'true'
+			else:
+				newStatus='false'
+			self.status=newStatus
+		if self.status is 'false' or not self.status and self.timed:
+			thedata = {'status':newStatus,'timed':'false','updatedAt':now}
+		else:
+			thedata = {'status':newStatus,'updatedAt':now}
+		req.put(baseLightsUrl + '/' + self.id, data=thedata, headers = header)
+			
+		
 	# perform check to determine if current time is within timed range
-	def inTimeRange(self, cur_hour, cur_minute):
-		on = time.strptime(self.onTime, '%H:%M')
-		off = time.strptime(self.offTime, '%H:%M')
+	def inTimeRange(self):
+		cur_hr = datetime.datetime.now().hour
+		cur_min = datetime.datetime.now().minute
+		on = time.strptime(self.onTime, '%H:%M:%S')
+		off = time.strptime(self.offTime, '%H:%M:%S')
+		start = datetime.time(on.tm_hour, on.tm_min, 0)
+		end = datetime.time(off.tm_hour, off.tm_min, 0)
+		cur = datetime.time(cur_hr, cur_min, 0)
 		
-		cur_time = cur_hour * 100 + cur_minute
-		on_time = on.tm_hour * 100 + on.tm_min
-		off_time = off.tm_hour * 100 + off.tm_min
-		
-		return cur_time >= on_time or cur_time < off_time
+		if start <= end:
+			return start <= cur < end
+		else:
+			return start <= cur or cur < end
 			
 """ end of class Light """
 
 # callback function to be executed on rising GPIO edge from PIR
 # toggles motion variable to true and toggles lights which are detecting motion to true
-def toggle_motion(channel):
+def toggle_motion():
 	print 'motion toggled on'
 	global cur_motion
 	global off_time
@@ -161,82 +193,71 @@ def toggle_motion(channel):
 	off_time = datetime.datetime.now() + datetime.timedelta(seconds=5)
 	for i in range(count):
 		if new_lights_list[i].motion:					# if a light is set to toggle on motion
-			new_lights_list[i].physical_toggle(newStatus='true')	# toggle that light to ON
+			new_lights_list[i].toggle_from_button(newStatus=True)	# toggle that light to ON
 			
 def check_expired_motion():
 	global cur_motion
 	global off_time
 	global new_lights_list
-	if datetime.datetime.now() > off_time:				# if time has elapsed
+	if datetime.datetime.now() > off_time and not GPIO.event_detected(pir_in):				# if time has elapsed
 		print 'motion expired'
 		for i in range(count):		
 			if new_lights_list[i].motion:					# if a light is set to toggle on motion
-				new_lights_list[i].physical_toggle(newStatus='false')	# toggle that light to OFF
+				new_lights_list[i].toggle_from_button()	# toggle that light to OFF
 		cur_motion = False
 
-# callback function to be executed on rising GPIO edge from a light
-# gets light to be toggled and toggles that light to opposite status
-def toggle_light(channel):
-	print 'toggle_light'
-	index = gpio_in.index(channel)
-	global new_lights_list
-	curStatus = new_lights_list[index].status
-	if curStatus is 'true':
-		new_lights_list[index].physical_toggle(newStatus='false')
-	else:
-		new_lights_list[index].physical_toggle(newStatus='true')
-
-try:
-	lights = json.loads(req.get(baseLightsUrl, headers=header).text)
-	GPIO.add_event_detect(pir_in, GPIO.RISING, callback=toggle_motion)
+try:	
+	GPIO.add_event_detect(pir_in, GPIO.RISING, bouncetime=250)
 	for i in range(count):
 		# add event detection for lights
 		# will pass a 'channel' - use to get index of init_gpio array and thus lights array
-		GPIO.add_event_detect(gpio_in[i], GPIO.RISING, callback=toggle_light, bouncetime=250)
-		# initialize Light objects from lights json
 		lights_list[i] = Light(lights[i]['_id'], lights[i]['description'], init_gpio_in[i], init_gpio_out[i], lights[i]['status'], lights[i]['onMotion'], lights[i]['timed'], lights[i]['onTime'], lights[i]['offTime'])
+		GPIO.add_event_detect(init_gpio_in[i], GPIO.RISING, bouncetime=250)
+		
+	print 'running'
 		
 	# begin infinite loop
 	while True:
-		cur_hour = datetime.datetime.now().hour
-		cur_min = datetime.datetime.now().minute
-		
 		new_lights = json.loads(req.get(baseLightsUrl, headers=header).text)
 		
-		for i in range(count):
-			new_lights_list[i] = Light(new_lights[i]['_id'], new_lights[i]['description'], init_gpio_in[i], init_gpio_out[i], new_lights[i]['status'], new_lig	hts[i]['onMotion'], new_lights[i]['timed'], new_lights[i]['onTime'], new_lights[i]['offTime'])
-			
+		if GPIO.event_detected(pir_in):
+			#print 'motion event detected'
+			toggle_motion()
+
 		# detect changes in api
-		for i in new_lights_list:
+		for i in range(count):
+			new_lights_list[i] = Light(new_lights[i]['_id'], new_lights[i]['description'], init_gpio_in[i], init_gpio_out[i], new_lights[i]['status'], new_lights[i]['onMotion'], new_lights[i]['timed'], new_lights[i]['onTime'], new_lights[i]['offTime'])
+
+			if GPIO.event_detected(init_gpio_in[i]):
+				print 'event detected on light ' + new_lights_list[i].description
+				new_lights_list[i].toggle_from_button()
+			
+			
 			# if a light is timed and current time is within its time range
-			if new_lights_list[i].timed and new_lights_list[i].inTimeRange(cur_hour, cur_min):
+			if new_lights_list[i].timed and new_lights_list[i].inTimeRange():
 				# if the light is currently off, toggle on
-				if new_lights_list[i].status is 'false':
-					new_lights_list[i].api_toggle(newStatus='true')
-					continue
+				#print 'in time range'
+				if not new_lights_list[i].status:
+					new_lights_list[i].toggle_from_button()
+			elif new_lights_list[i].timed and not new_lights_list[i].inTimeRange():
+				#print 'no longer in time range'
+				if new_lights_list[i].status:
+					new_lights_list[i].toggle_from_button()
+
 			# if light status is not equal to new status, toggle to new status
-			if lights_list[i].status is not new_lights_list[i].status:
-				lights_list[i].api_toggle(newStatus=new_lights_list[i].status)
-				continue
+			new_lights_list[i].api_toggle(newStatus=new_lights_list[i].status)
 			# if light motion is not equal to new motion, toggle to new motion
-			if lights_list[i].motion is not new_lights_list[i].motion:
-				lights_list[i].api_toggle(newMotion=new_lights_list[i].motion)
-				continue
+			new_lights_list[i].api_toggle(newMotion=new_lights_list[i].motion)
 			# if light timed is not equal to new timed, toggle to new timed
-			if lights_list[i].timed is not new_lights_list[i].timed:
-				lights_list[i].api_toggle(newTimed=new_lights_list[i].timed)
-				continue
+			new_lights_list[i].api_toggle(newTimed=new_lights_list[i].timed)
 			# if light onTime is not equal to new onTime, update
-			if lights_list[i].onTime is not new_lights_list[i].onTime:
-				lights_list[i].api_toggle(newOnTime=new_lights_list[i].onTime)
+			new_lights_list[i].api_toggle(newOnTime=new_lights_list[i].onTime)
 			# if light offTime is not equal to new offTime, update
-			if lights_list[i].offTime is not new_lights_list[i].offTime:
-				lights_list[i].api_toggle(newOffTime=new_lights_list[i].offTime)
-				
+			new_lights_list[i].api_toggle(newOffTime=new_lights_list[i].offTime)
+
 		if cur_motion:				# if motion has been detected
 			check_expired_motion()	# shut off if time has elapsed
-		lights_list = new_lights_list
-	
+		
 except KeyboardInterrupt:
 	print '\nKeyboardInterruptException'
 
